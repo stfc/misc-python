50a947d197b299fe19536773bd018162fca0d174
apdibbo <apdibbo@clearskies.it>
2016-06-13 08:47:29 +0100

    Additional work

--- regex-hangman-v1.py
+++ regex-hangman-v1.py
@@ -1,11 +1,16 @@
 import re
 import random
 
+maxlength=4
+easymode=True
+
 with open("dictionary.txt", "r") as dictionary:
     lines = dictionary.read().splitlines()
 
 def getRandomWord(lines):
-    word = lines[random.randrange(0, len(lines))]
+    word = ""
+    while len(word) > maxlength or word == "":
+        word = lines[random.randrange(0, len(lines))]
 
     return word
 
@@ -14,9 +19,11 @@ class RegExHangman:
     def __init__(self, word):
         self.origWord = word
         self.word = "_" * (len(word))
-        self.prevGuesses = []
+        self.prevGuesses = []        
+        self.matchGuesses = []
+        self.missGuesses = []
         self.letters = []
-        self.limit = 10
+        self.limit = 20
         self.main()
 
     def getWord(self):
@@ -32,22 +39,28 @@ class RegExHangman:
         return self.limit
 
     def getRegEx(self):
-        regString = input("Enter a regular expression: ")
-
+        print("You have " + str(self.limit) + " tries remaining")
+        print("Missed Guesses: " + str(self.missGuesses))
+        print("Matched Guesses: " + str(self.matchGuesses))
+        regString = raw_input("Enter a regular expression or a guess: ")
         while regString in self.prevGuesses:
             print("You already guessed " + regString + "!")
             regString = input("Enter a regular expression: ")
 
         self.prevGuesses.append(regString)
-        search = re.search(regString, self.origWord)
-
+        search = re.match(regString, self.origWord)
+        if search != None:
+            self.matchGuesses.append(regString)
+        else:
+            self.missGuesses.append(regString)
+        print(search)
         return search
 
     def buildWord(self, letters):
         tempWord = ""
 
         for i in range(len(self.origWord)):
-            if self.origWord[i] in letters:
+            if easymode and i <2 :
                 tempWord += self.origWord[i]
             else:
                 tempWord += "_"
@@ -56,8 +69,8 @@ class RegExHangman:
 
     def main(self):
         print(self.word + " (" + str(len(self.origWord)) + " characters)")
-        
-        while self.word != self.origWord and self.limit != 0:
+        regEx=""
+        while regEx != self.origWord and self.limit != 0:
             regEx = self.getRegEx()
             
             if regEx:
@@ -67,13 +80,17 @@ class RegExHangman:
                 self.buildWord(self.letters)
                 print(self.word)
             else:
-                print("No matches!\n" + self.word)
+                print("No matches!\n" + str(regEx))
 
             self.limit -= 1
-
-        if self.word == self.origWord:
+        
+        if regEx == self.origWord:
             print("You won!\n" + self.origWord + " guessed correctly!")
         else:
-            print("You lost! \nThe word was " + self.origWord)
+            guess = raw_input("Last chance, please enter a guess: ")
+            if guess == self.origWord:
+                print("Congratulations you figured it out... somehow")
+            else:
+                print("Bad luck. The word is: " + self.origWord)
 
 hangman = RegExHangman(getRandomWord(lines))                  
